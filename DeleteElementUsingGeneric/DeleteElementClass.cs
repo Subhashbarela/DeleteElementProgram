55860177df7fae7e6cea86cbc1e8c52e07bb4f0d
Subhash Barela <barelasubhash16@gmail.com>
2023-07-06 17:28:40 +0530

    [subhash] Delete element in array using normal class

---
+++ DeleteElementUsingGeneric/DeleteElementClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteElementUsingGeneric
+{
+    public class DeleteElementClass
+    {
+        public void DeleteMethod(int[] intArray, int pos)
+        {
+            Console.WriteLine("Before Delete the element");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+
+            for (int i = pos - 1; i < intArray.Length - 1; i++)
+            {
+                intArray[i] = intArray[i + 1];
+            }
+            Console.WriteLine("\nAfter Delete the element");
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+            Console.WriteLine("\n............................");
+        }
+        public void DeleteMethod(double[] intArray, int pos)
+        {
+            Console.WriteLine("Before Delete the element");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+
+            for (int i = pos - 1; i < intArray.Length - 1; i++)
+            {
+                intArray[i] = intArray[i + 1];
+            }
+            Console.WriteLine("\nAfter Delete the element");
+
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+            Console.WriteLine("\n..............................");
+        }
+        public void DeleteMethod(char[] intArray, int pos)
+        {
+            Console.WriteLine("Before Delete the element");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+
+            for (int i = pos - 1; i < intArray.Length - 1; i++)
+            {
+                intArray[i] = intArray[i + 1];
+            }
+            Console.WriteLine("\nBefore Delete the element");           
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                Console.Write(intArray[i] + " ");
+            }
+        }
+    }
+}
--- DeleteElementUsingGeneric/Program.cs
+++ DeleteElementUsingGeneric/Program.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace DeleteElementUsingGeneric
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            DeleteElementClass delete = new DeleteElementClass();
+            int[] intArr = { 10, 20, 30, 40, 50, 60 };
+            delete.DeleteMethod(intArr, 3);
+            double[] doubleArr = { 10.3, 20.4, 30.5, 40.3, 50.11, 60.8 };
+            delete.DeleteMethod(doubleArr, 3);
+            char[] stringArr = { 'A','B','C','D','E','F'};
+            delete.DeleteMethod(stringArr, 3);
+        Console.ReadLine();
         }
     }
 }
